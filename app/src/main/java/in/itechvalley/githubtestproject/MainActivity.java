1c13841b2b2a1698d80c443c525a87702ac9e836
zackygaurav <zacky.gaurav@gmail.com>
2017-01-06 17:33:55 +0530

    Initial commit

---
+++ app/src/main/java/in/itechvalley/githubtestproject/MainActivity.java
@@ -0,0 +1,15 @@
+package in.itechvalley.githubtestproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
